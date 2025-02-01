41e56ede0ce66ea8596dec9d2aca5fcb43a5830d
Fir <czg@euclidolap.com>
2025-02-01 02:20:21 +0000

    Changes to be committed: 	modified:   .gitignore 	new file:   package.json 	new file:   src/index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// index.js
+console.log('Aggregation Engine Agent is running.............................................................!');
+
+// 持续运行进程
+setInterval(() => {
+  console.log('>> Aggregation Engine Agent << running...');
+}, 1000);
+
